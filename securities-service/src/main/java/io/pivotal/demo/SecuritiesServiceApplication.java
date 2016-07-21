5c74e8f0cdee8ea63e89b7167c78be27152a0121
Marcial Rosales <mrosales@pivotal.io>
2016-07-21 21:02:33 +0200

    add first projects to show config server in action with a simple securities-service

---
+++ securities-service/src/main/java/io/pivotal/demo/SecuritiesServiceApplication.java
@@ -0,0 +1,37 @@
+package io.pivotal.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@SpringBootApplication
+public class SecuritiesServiceApplication {
+
+    @Bean
+    CommandLineRunner runner(SecuritiesRepository repo) {
+        return args ->
+                Arrays.asList("EURUSD,USD;GBPUSD,USD;CHFEUR,EUR".split(";"))
+                        .forEach(s -> {
+                        	String[] security = s.split(",");
+                        	repo.save(new Security(security[0], security[1]));
+                        });
+
+    }
+    
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritiesServiceApplication.class, args);
+	}
+}
+
+
+@RepositoryRestResource(path = "/securities")
+interface SecuritiesRepository extends JpaRepository<Security, Long> {
+	List<Security> findBySymbol(@Param("symbol") String symbol);
+}
